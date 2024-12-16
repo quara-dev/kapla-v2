0af36c3e185358e02a5b2cff826b0ef887b9e5f0
Aurelien Pisu <aurelien.pisu@araymond.com>
2024-12-16 10:47:36 +0100

    fix

--- src/kapla/core/logger.py
+++ src/kapla/core/logger.py
@@ -15,14 +15,12 @@ def configure_logger(level: Optional[int] = None) -> None:
     """
     # Check that the logging level exists
     # Optionally fetch logging level from environment variable
-    level_: int
-    if level is not None:
-        level_ = level
-    else:
+    if level is None:
         level_str = os.environ.get("LOGGING_LEVEL", NOTSET)
         level_ = logging.getLevelName(level_str)
-        if not isinstance(level, int):
+        if not isinstance(level_, int):
             raise ValueError(f"Logging level not supported: {level_}")
+        level = level_
     structlog.configure(
         processors=[
             structlog.processors.add_log_level,
@@ -31,7 +29,7 @@ def configure_logger(level: Optional[int] = None) -> None:
             structlog.processors.TimeStamper(fmt="iso", utc=True),
             structlog.dev.ConsoleRenderer(),
         ],
-        wrapper_class=structlog.make_filtering_bound_logger(level_),
+        wrapper_class=structlog.make_filtering_bound_logger(level),
         context_class=dict,
         logger_factory=structlog.PrintLoggerFactory(),
         cache_logger_on_first_use=False,
